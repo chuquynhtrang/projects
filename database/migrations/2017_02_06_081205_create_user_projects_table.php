3a8a8d228c82940dde4a2eb94d292bc3e47ff403
chu.quynh.trang <chu.quynh.trang@FRAMGIA.COM>
2017-02-06 15:23:06 +0700

    create project

---
+++ database/migrations/2017_02_06_081205_create_user_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('project_id');
+            $table->integer('progress')->nullable();
+            $table->string('document');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_projects', function (Blueprint $table) {
+            Schema::dropIfExists('user_projects');
+        });
+    }
+}
